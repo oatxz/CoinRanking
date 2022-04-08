5bbc955419262331c4e885a7849456397fbc2c77
Ext Sitthiphong <ext.sitthipong.kan@truedigital.com>
2022-04-08 17:47:38 +0700

    Initial Commit

---
+++ Shared/LMWN_CoinrankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMWN_CoinrankingApp.swift
+//  Shared
+//
+//  Created by Oatxz on 8/4/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct LMWN_CoinrankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
